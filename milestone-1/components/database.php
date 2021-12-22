85be0b980b542bbfdf16a11e55416b07a22b035a
Mdigennaro <michele.digennaro719@gmail.com>
2021-12-22 18:35:44 +0100

    creazione scheletro

---
+++ milestone-1/components/database.php
@@ -0,0 +1,91 @@
+<?php
+
+//Creo l'array contenente i dischi
+$albums = [
+  [
+    "poster" => "https://www.moma.org/media/W1siZiIsIjMwNTU2MiJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA5MCAtcmVzaXplIDIwMDB4MjAwMFx1MDAzZSJdXQ.jpg?sha=25104b3c296900be",
+    "title" => "Sgt. Pepper's Lonely Hearts Club Band",
+    "author" => "The Beatles",
+    "genre" => "Rock",
+    "year" => 1967
+  ],
+  [
+    "poster" => "https://upload.wikimedia.org/wikipedia/en/b/bb/PetSoundsCover.jpg",
+    "title" => "Pet Sounds",
+    "author" => "The Beach Boys",
+    "genre" => "Pop",
+    "year" => 1966
+  ],
+  [
+    "poster" => "https://m.media-amazon.com/images/I/91ffeWzPNpL._AC_SL1500_.jpg",
+    "title" => "Revolver",
+    "author" => "The Beatles",
+    "genre" => "Rock",
+    "year" => 1966
+  ],
+  [
+    "poster" => "https://img.discogs.com/-F-9ToqOUCue4EB6Bve-BPFahbQ=/fit-in/600x598/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-9619261-1483738742-5536.jpeg.jpg",
+    "title" => "Highway 61 Revisited",
+    "author" => "Bob Dylan",
+    "genre" => "Rock",
+    "year" => 1965
+  ],
+  [
+    "poster" => "https://upload.wikimedia.org/wikipedia/en/7/74/Rubber_Soul.jpg",
+    "title" => "Rubber Soul",
+    "author" => "The Beatles",
+    "genre" => "Rock",
+    "year" => 1965
+  ],
+  [
+    "poster" => "https://m.media-amazon.com/images/I/719kIe1npJL._AC_SX679_.jpg",
+    "title" => "What's Going On",
+    "author" => "Marvin Gaye",
+    "genre" => "Pop",
+    "year" => 1971
+  ],
+  [
+    "poster" => "https://upload.wikimedia.org/wikipedia/en/c/ca/ExileMainSt.jpg",
+    "title" => "Exile on Main St.",
+    "author" => "The Rolling Stones",
+    "genre" => "Blues rock",
+    "year" => 1972
+  ],
+  [
+    "poster" => "https://upload.wikimedia.org/wikipedia/it/3/3c/London_Calling.jpg",
+    "title" => "London Calling",
+    "author" => "The Clash",
+    "genre" => "Punk rock",
+    "year" => 1979
+  ],
+  [
+    "poster" => "https://img.discogs.com/J_8aiRMJBAzpK3RxadwxZiSUn9E=/fit-in/600x599/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4633671-1370551123-1969.jpeg.jpg",
+    "title" => "Blonde on Blonde",
+    "author" => "Bob Dylan",
+    "genre" => "Rock",
+    "year" => 1966
+  ],
+  [
+    "poster" => "https://upload.wikimedia.org/wikipedia/commons/0/0c/Sleeve_of_the_Beatles%27_White_Album.svg",
+    "title" => "The Beatles (The White Album)",
+    "author" => "The Beatles",
+    "genre" => "Hard rock",
+    "year" => 1968
+  ],
+  [
+    "poster" => "https://upload.wikimedia.org/wikipedia/en/c/cd/Elvis_Presley_The_Sun_Sessions_LP_Cover.jpg",
+    "title" => "The Sun Sessions",
+    "author" => "Elvis Presley",
+    "genre" => "Rock",
+    "year" => 1976
+  ],
+  [
+    "poster" => "https://upload.wikimedia.org/wikipedia/en/9/9c/MilesDavisKindofBlue.jpg",
+    "title" => "Kind of Blue",
+    "author" => "Miles Davies",
+    "genre" => "Jazz",
+    "year" => 1959
+  ],
+]
+
+?>
--- milestone-1/index.php
+++ milestone-1/index.php
@@ -1,5 +1,10 @@
 <?php
 
+include_once __DIR__ . './components/database.php';
+
+var_dump($albums)
+
+
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +13,31 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/style.css">
   <title>Document</title>
 </head>
 <body>
+
+<header>
+
+<div class="header-left">
+  <img src="img/spotify-logo.png" alt="logo">
+</div>
+<div class="header-right"></div>
+  
+</header>
+
+<main>
+  <div class="container-album">
+    
+  <?php foreach($albums as $album) ?>
+    <div class='box-album'></div>
+  <?php  endforeach ?>
+
+     
+  </div>
+</main>
+<footer></footer>
   
 </body>
 </html>
