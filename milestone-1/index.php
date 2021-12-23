de98641d5950332ece16332f6f616e0d7bd86360
Mdigennaro <michele.digennaro719@gmail.com>
2021-12-23 12:17:10 +0100

    fine main

--- milestone-1/index.php
+++ milestone-1/index.php
@@ -2,8 +2,6 @@
 
 include_once __DIR__ . './components/database.php';
 
-var_dump($albums)
-
 
 ?>
 
@@ -13,8 +11,18 @@ var_dump($albums)
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style/style.css">
-  <title>Document</title>
+
+  <!--Font-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
+  <!--/Font-->
+
+  <!--CSS-->
+  <link rel="stylesheet" href="style/style.css?ts=<?=time()?>&quot">
+  <!--/CSS-->
+
+  <title>Dischi</title>
 </head>
 <body>
 
@@ -23,6 +31,7 @@ var_dump($albums)
 <div class="header-left">
   <img src="img/spotify-logo.png" alt="logo">
 </div>
+
 <div class="header-right"></div>
   
 </header>
@@ -30,9 +39,20 @@ var_dump($albums)
 <main>
   <div class="container-album">
     
-  <?php foreach($albums as $album) ?>
-    <div class='box-album'></div>
-  <?php  endforeach ?>
+  <?php foreach($albums as $album): ?>
+    <div class="box-album">
+    
+      <?php 
+
+        echo "<a href='{$album['poster']}'><img src='{$album['poster']}'></a>";
+        echo "<a href='{$album['poster']}'><h2>{$album['title']}</h2></a>";
+        echo "<h3>{$album['author']}</h3>";
+        echo "<h5>{$album['year']}</h5>";
+
+      ?>
+
+    </div>
+  <?php  endforeach; ?>
 
      
   </div>
